ae2d6e6b7fcf1e5674c60d31ca0131b712fe1160
Vishwajeet Koshti <vishwajeetskoshti2003@gmail.com>
2025-01-15 01:55:47 +0530

    Created a new Expo app

---
+++ components/Button.js
@@ -0,0 +1,67 @@
+import { Dimensions, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+export default ({ onPress, text, size, theme }) => {
+  const buttonStyles = [styles.button];
+  const textStyles = [styles.text];
+
+  if (size === "double") {
+    buttonStyles.push(styles.buttonDouble);
+  }
+  if( theme === "green") {
+    buttonStyles.push(styles.buttonGreen);
+    // textStyles.push(styles.textGreen);
+  }
+
+  if (theme === "secondary") {
+    buttonStyles.push(styles.buttonSecondary);
+    textStyles.push(styles.textSecondary);
+  } else if (theme === "accent") {
+    buttonStyles.push(styles.buttonAccent);
+  }
+
+  return (
+    <TouchableOpacity onPress={onPress} style={buttonStyles}>
+      <Text style={textStyles}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+// set dimmenstion
+const screen = Dimensions.get("window");
+const buttonWidth = screen.width / 4;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#333333",
+    flex: 1,
+    height: Math.floor(buttonWidth - 10),
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: Math.floor(buttonWidth),
+    margin: 5,
+    // border-radius: 22px;
+    background:" #373434",
+    boxShadow:"  -8px 8px 16px #211f1f,8px -8px 16px #4d4949",},
+  text: {
+    color: "#fff",
+    fontSize: 24,
+  },
+  textSecondary: {
+    color: "#060606",
+  },
+  buttonDouble: {
+    width: screen.width / 2 - 10,
+    flex: 0,
+    alignItems: "flex-start",
+    paddingLeft: 40,
+  },
+  buttonSecondary: {
+    backgroundColor: "#a6a6a6",
+  },
+  buttonAccent: {
+    backgroundColor: "#ffc107",
+  },
+  buttonGreen: {
+    backgroundColor: "#85A947",
+  },
+});
